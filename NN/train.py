3b8b373326a26bbc1437c42e6c84f09e5743ee72
Roman Degtyarev <romandegtyarev@MacBook-Pro-Roman.local>
2018-05-04 15:44:18 +0300

    ok

--- NN/train.py
+++ NN/train.py
@@ -1,28 +1,72 @@
 from utils import ReaderSubmitor
 import numpy as np
-
-
+from keras.layers import Input, Dense, BatchNormalization, Activation
+import keras
+from keras.models import Model
+from keras import regularizers
+from keras import optimizers
+from tqdm import tqdm
 
 
 class Network:
-	def __init__(self):
-		pass
-	def build_model(self):
-		pass
-	def fit(self):
-		pass
-	def predict(self):
-		pass
+    def __init__(self):
+        self.model = None
+
+    def build_model(self, hidden_layers, X_train_shape1, reg_lam=0.001):
+        x01 = Input(shape=(X_train_shape1,))
+        x1 = Dense(hidden_layers[0], kernel_regularizer=regularizers.l2(reg_lam))(x01)
+        x1 = BatchNormalization()(x1)
+        x1 = Activation('relu')(x1)
+
+        x02 = Input(shape=(X_train_shape1,))
+        x2 = Dense(hidden_layers[0], kernel_regularizer=regularizers.l2(reg_lam))(x02)
+        x2 = BatchNormalization()(x2)
+        x2 = Activation('relu')(x2)
+
+        x = keras.layers.dot([x1, x2], axes=1)
+
+        x3 = keras.layers.concatenate([x01, x02], axis=-1)
+        x4 = Dense(hidden_layers[1])(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = Activation('relu')(x4)
+
+        x_out = keras.layers.concatenate([x, x4], axis=-1)
+        # x = Flatten()(x)
+        x = Dense(1)(x_out)
+        x = Activation('sigmoid')(x)
+        model = Model(inputs=[x01, x02], output=x)
+        self.model = model
+        model.summary()
+
+    def fit(self, X_train, y_train, learning_rate=0.01, batch_size=500, niter=10):
+        if self.model is None:
+            print('first you need build_model()')
+        else:
+            self.model.compile(optimizer=optimizers.Adam(lr=learning_rate), loss='binary_crossentropy',
+                               metrics=['accuracy'])
+            self.model.fit(X_train, y_train, batch_size=batch_size, niter=niter)
+
+    def predict(self, X_test):
+        y_pred = self.model.predict(X_test)
+        return y_pred
+
 
 def main():
-	reader = ReaderSubmitor()
-	all_values = reader.get_values()
-	chunk = reader.next_chunk(all_values=all_values, chunksize=100)
-	X_train, y_train = next(chunk)
-	matrix = X_train.todense()
-	print(matrix.shape)
-	print('here')
+    reader = ReaderSubmitor()
+    all_values, data_len = reader.get_values(nrows=1000)
+    model = Network()
+
+    chunk_temp = reader.next_chunk(all_values=all_values, chunksize=1)
+    X_train_temp, y_train_temp = next(chunk_temp)
+    matrix = X_train_temp.todense()
+
+    model.build_model(hidden_layers=[32,16], X_train_shape1=matrix.shape[1])
+
+    for chunk in tqdm(reader.next_chunk(all_values=all_values, chunksize=100)):
+        X_train, y_train = next(chunk)
+        #X_train_numpy = X_train.todense()
+        model.fit(X_train, y_train)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- NN/utils.py
+++ NN/utils.py
@@ -9,69 +9,70 @@ from sklearn import preprocessing
 
 
 class ReaderSubmitor:
-	def __init__(self):
-		self.train_path = '../data/train.csv'
-		self.test_path = '../data/test/csv'
-		self.train_cols = ['timestamp', 'C1', 'C2', 'C3', 'C4',
-		                   'C5', 'C6', 'C7', 'C9',
-		                   'C10', 'C11', 'C12', 'l1', 'l2']
+    def __init__(self):
+        self.train_path = '../data/train.csv'
+        self.test_path = '../data/test/csv'
+        self.train_cols = ['timestamp', 'C1', 'C2', 'C3', 'C4',
+                           'C5', 'C6', 'C7', 'C9',
+                           'C10', 'C11', 'C12', 'l1', 'l2']
 
-		self.label_bin = preprocessing.LabelBinarizer(sparse_output=True)
-		self.chunk_count = 0
+        self.label_bin = preprocessing.LabelBinarizer(sparse_output=True)
+        self.chunk_count = 0
 
-	def get_values(self):
-		data = pd.read_csv(self.train_path, sep=";", nrows=1000)
-		all_values = dict()
-		print('Start counting values in column')
-		for column in tqdm(data.columns):
-			all_values[column] = set(data[column])
-		return all_values
+    def get_values(self, nrows = 1000):
+        if nrows == -1:
+            data = pd.read_csv(self.train_path, sep=";")
+        else:
+            data = pd.read_csv(self.train_path, sep=";", nrows=nrows)
 
+        data_len = data.shape[0]
+        all_values = dict()
+        print('Start counting values in column')
+        for column in tqdm(data.columns):
+            all_values[column] = set(data[column])
+        return all_values, data_len
 
-	def prepare_one_hot(self, data_column, col_values):
-		value_key = dict({value: i for i, value in enumerate(col_values)})
-		one_hot_array = np.zeros((len(data_column), len(col_values)))
-		for i, raw in enumerate(data_column.values):
-			value = np.zeros((len(col_values),))
-			temp_one = value_key[raw]
-			value[temp_one] = 1
-			one_hot_array[i] = value
-		one_hot_csr = csr_matrix(one_hot_array)
-		return one_hot_csr
+    def prepare_one_hot(self, data_column, col_values):
+        value_key = dict({value: i for i, value in enumerate(col_values)})
+        one_hot_array = np.zeros((len(data_column), len(col_values)))
+        for i, raw in enumerate(data_column.values):
+            value = np.zeros((len(col_values),))
+            temp_one = value_key[raw]
+            value[temp_one] = 1
+            one_hot_array[i] = value
+        one_hot_csr = csr_matrix(one_hot_array)
+        return one_hot_csr
 
+    def next_chunk(self, all_values, chunksize=1000000):
+        for batch in pd.read_csv(self.train_path, sep=";", chunksize=chunksize):
+            X_train = batch[self.train_cols]
+            y_train = batch['label']
+            train_csr = None
+            print('Start making chunk {}'.format(self.chunk_count))
+            self.chunk_count += 1
+            for j, column in enumerate(self.train_cols):
+                if j == 0:
+                    train_csr = self.prepare_one_hot(X_train[column], all_values[column])
+                else:
+                    one_hot_csr = self.prepare_one_hot(X_train[column], all_values[column])
+                    train_csr = hstack([train_csr, one_hot_csr])
+            yield train_csr, y_train
 
-	def next_chunk(self, all_values, chunksize=1000000):
-		for batch in pd.read_csv(self.train_path, sep=";", chunksize=chunksize):
-			X_train = batch[self.train_cols]
-			y_train = batch['label']
-			train_csr = None
-			print('Start making chunk {}'.format(self.chunk_count))
-			self.chunk_count+=1
-			for j, column in enumerate(self.train_cols):
-				if j == 0:
-					train_csr = self.prepare_one_hot(X_train[column], all_values[column])
-				else:
-					one_hot_csr = self.prepare_one_hot(X_train[column], all_values[column])
-					train_csr = hstack([train_csr, one_hot_csr])
-			yield train_csr, y_train
-
-
-	def read_test(self):
-		test_data = pd.read_csv(self.test_path, sep=";")
-		X_test = test_data[self.train_cols]
-		del test_data
-		gc.collect()
-		return X_test
+    def read_test(self):
+        test_data = pd.read_csv(self.test_path, sep=";")
+        X_test = test_data[self.train_cols]
+        del test_data
+        gc.collect()
+        return X_test
 
 
 def make_prediction(preds_proba):
-	predictions = preds_proba[:, 1]
-	print("start making prediction file")
-	n = 0
-	with open('./data/result_{}.txt'.format(time.time()), 'w') as f:
-		f.write('Id,Click\n')
-		for i in tqdm(range(1, len(predictions) + 1)):
-			n += 1
-			f.write("{},{}\n".format(i, predictions[i - 1]))
-	print("result ready...", n)
-
+    predictions = preds_proba[:, 1]
+    print("start making prediction file")
+    n = 0
+    with open('./data/result_{}.txt'.format(time.time()), 'w') as f:
+        f.write('Id,Click\n')
+        for i in tqdm(range(1, len(predictions) + 1)):
+            n += 1
+            f.write("{},{}\n".format(i, predictions[i - 1]))
+    print("result ready...", n)
